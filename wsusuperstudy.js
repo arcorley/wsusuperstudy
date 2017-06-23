28d49f5dc40a86c82b705a64f832ac4ee7cd2828
arcorley <arcorley528@gmail.com>
2017-06-23 14:33:35 -0700

    Update wsusuperstudy.js
    Added the rest of the option fields

--- wsusuperstudy.js
+++ wsusuperstudy.js
@@ -53,8 +53,8 @@ app.get('/add-new', function(req,res,next){
 
 /**************INSERT NEW PARTICIPANT IN DATABASE******************/
 app.post('/insert-ppt', function(req, res, next){
-	mysql.pool.query("INSERT INTO super_study.participants_temp (`ppt_first_nm`,`ppt_last_nm`,`ppt_cell_phone_no`,`ppt_home_phone_no`,`ppt_email`,`addr_line_1`,`addr_line_2`,`addr_city`,`addr_state`,`addr_zip`,`referred_by`,`referred_by_other`,`future_contact`,`future_contact_method`,`partner_interest`,`partner_first_nm`,`partner_last_nm`,`partner_cell_phone_no`,`partner_home_phone_no`,`partner_email`,`english_check`,`ppt_age`,`partner_age`,`married_flag`,`rel_length`,`living_together`,`living_together_length`,`ppt_daily_pain`,`ppt_pain_length`,`ppt_pain_location`,`ppt_pain_level`,`ppt_pain_interference`,`partner_daily_pain`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-	[req.body.ppt_first_nm, req.body.ppt_last_nm, req.body.ppt_cell_phone_no, req.body.ppt_home_phone_no, req.body.ppt_email,req.body.addr_line_1, req.body.addr_line_2, req.body.addr_city, req.body.addr_state, req.body.addr_zip, req.body.referred_by, req.body.referred_by_other, req.body.future_contact, req.body.future_contact_method, req.body.partner_interest, req.body.partner_first_nm, req.body.partner_last_nm, req.body.partner_cell_phone_no, req.body.partner_home_phone_no, req.body.partner_email, req.body.english_check, req.body.ppt_age, req.body.partner_age, req.body.married_flag, req.body.rel_length, req.body.living_together, req.body.living_together_length, req.body.ppt_daily_pain, req.body.ppt_pain_length, req.body.ppt_pain_location, req.body.ppt_pain_level, req.body.ppt_pain_interference, req.body.partner_daily_pain],
+	mysql.pool.query("INSERT INTO super_study.participants_temp (`ppt_first_nm`,`ppt_last_nm`,`ppt_cell_phone_no`,`ppt_home_phone_no`,`ppt_email`,`addr_line_1`,`addr_line_2`,`addr_city`,`addr_state`,`addr_zip`,`referred_by`,`referred_by_other`,`future_contact`,`future_contact_method`,`partner_interest`,`partner_first_nm`,`partner_last_nm`,`partner_cell_phone_no`,`partner_home_phone_no`,`partner_email`,`english_check`,`ppt_age`,`partner_age`,`married_flag`,`rel_length`,`living_together`,`living_together_length`,`ppt_daily_pain`,`ppt_pain_length`,`ppt_pain_location`,`ppt_pain_level`,`ppt_pain_interference`,`partner_daily_pain`,`completed_screen`,`num_contacts`,`ineligible_no_full_screen`,`disinterest`,`eligibility_status`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+	[req.body.ppt_first_nm, req.body.ppt_last_nm, req.body.ppt_cell_phone_no, req.body.ppt_home_phone_no, req.body.ppt_email,req.body.addr_line_1, req.body.addr_line_2, req.body.addr_city, req.body.addr_state, req.body.addr_zip, req.body.referred_by, req.body.referred_by_other, req.body.future_contact, req.body.future_contact_method, req.body.partner_interest, req.body.partner_first_nm, req.body.partner_last_nm, req.body.partner_cell_phone_no, req.body.partner_home_phone_no, req.body.partner_email, req.body.english_check, req.body.ppt_age, req.body.partner_age, req.body.married_flag, req.body.rel_length, req.body.living_together, req.body.living_together_length, req.body.ppt_daily_pain, req.body.ppt_pain_length, req.body.ppt_pain_location, req.body.ppt_pain_level, req.body.ppt_pain_interference, req.body.partner_daily_pain, req.body.completed_screen, req.body.num_contacts, req.body.ineligible_no_full_screen, req.body.disinterest, req.body.eligibility_status],
 	function(err, result){
 		if(err){
 			next(err);
@@ -269,8 +269,38 @@ app.get('/in_progress_detail', function(req, res, next){
 				" WHERE a.field_nm = 'ppt_pain_interference';" +
 			" SELECT a.*, case when pd.partner_daily_pain IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
 				" LEFT JOIN (SELECT partner_daily_pain FROM super_study.participants_temp WHERE ppt_id=?) pd ON a.option_nm=pd.partner_daily_pain" +
-				" WHERE a.field_nm = 'partner_daily_pain';"
-			,[req.query.ppt_id, req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id], function(err, rows, fields){
+				" WHERE a.field_nm = 'partner_daily_pain';" + 
+			" SELECT a.*, case when pl.partner_pain_length IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT partner_pain_length FROM super_Study.participants_temp WHERE ppt_id=?) pl ON a.option_nm=pl.partner_pain_length" +
+				" WHERE a.field_nm = 'partner_pain_length';" +
+			" SELECT a.*, case when part_pi.partner_pain_interference IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT partner_pain_interference from super_study.participants_temp WHERE ppt_id=?) part_pi ON a.option_nm=part_pi.partner_pain_interference" +
+				" WHERE a.field_nm = 'partner_pain_interference';" +
+			" SELECT a.*, case when cs.completed_screen IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT completed_screen from super_study.participants_temp WHERE ppt_id=?) cs ON a.option_nm=cs.completed_screen" +
+				" WHERE a.field_nm = 'completed_screen';" +
+			" SELECT a.*, case when infs.ineligible_no_full_screen IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT ineligible_no_full_screen from super_study.participants_temp WHERE ppt_id=?) infs ON a.option_nm=infs.ineligible_no_full_screen" +
+				" WHERE a.field_nm = 'ineligible_no_full_screen';" +
+			" SELECT a.*, case when dis.disinterest IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT disinterest from super_study.participants_temp WHERE ppt_id=?) dis ON a.option_nm=dis.disinterest" +
+				" WHERE a.field_nm='disinterest';" +
+			" SELECT a.*, case when es.eligibility_status IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT eligibility_status from super_study.participants_temp WHERE ppt_id=?) es ON a.option_nm=es.eligibility_status" +
+				" WHERE a.field_nm='eligibility_status';" + 
+			" SELECT a.*, case when infsr.ineligible_no_full_screen_reason IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT ineligible_no_full_screen_reason from super_study.participants_temp WHERE ppt_id=?) infsr ON a.option_nm=infsr.ineligible_no_full_screen_reason" +
+				" WHERE a.field_nm='ineligible_no_full_screen_reason';" +
+			" SELECT a.*, case when dr.disinterest_reason IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT disinterest_reason from super_study.participants_temp WHERE ppt_id=?) dr ON a.option_nm=dr.disinterest_reason" +
+				" WHERE a.field_nm='disinterest_reason';" +
+			" SELECT a.*, case when ppt.ppt_pain_type IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT ppt_pain_type from super_study.participants_temp WHERE ppt_id=?) ppt ON a.option_nm=ppt.ppt_pain_type" +
+				" WHERE a.field_nm='ppt_pain_type';" +
+			" SELECT a.*, case when part_pt.partner_pain_type IS NOT NULL then 'selected' else '' end as selected FROM super_study.options a" +
+				" LEFT JOIN (SELECT partner_pain_type from super_study.participants_temp WHERE ppt_id=?) part_pt ON a.option_nm = part_pt.partner_pain_type" +
+				" WHERE a.field_nm='partner_pain_type';"
+			,[req.query.ppt_id, req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id,req.query.ppt_id], function(err, rows, fields){
 				if(err){
 					next(err);
 					return;
@@ -281,6 +311,8 @@ app.get('/in_progress_detail', function(req, res, next){
 				rows[0].living_together_mos = Math.round(rows[0][0].living_together_length%12);
 				rows[0].ppt_pain_yrs = Math.round(rows[0][0].ppt_pain_length/12);
 				rows[0].ppt_pain_mos = Math.round(rows[0][0].ppt_pain_length%12);
+				rows[0].partner_pain_yrs = Math.round(rows[0][0].partner_pain_length/12);
+				rows[0].partner_pain_mos = Math.round(rows[0][0].partner_pain_length%12);
 				context.ppt_results = rows[0];
 				context.referred_by_options = rows[1];
 				context.future_contact = rows[2];
@@ -293,6 +325,16 @@ app.get('/in_progress_detail', function(req, res, next){
 				context.ppt_pain_level = rows[9];
 				context.ppt_pain_interference = rows[10];
 				context.partner_daily_pain = rows[11];
+				context.partner_pain_level = rows[12];
+				context.partner_pain_interference = rows[13];
+				context.completed_screen = rows[14];
+				context.ineligible_no_full_screen = rows[15];
+				context.disinterest = rows[16];
+				context.eligibility_status = rows[17];
+				context.ineligible_no_full_screen_reason = rows[18];
+				context.disinterest_reason = rows[19];
+				context.ppt_pain_type = rows[20];
+				context.partner_pain_type = rows[21];
 				res.render('in_progress_detail', context);
 			});
 		}
