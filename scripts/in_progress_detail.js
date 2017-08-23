e4a97b779a048d1a2a88922d04461ba08c5ec029
Austin Corley <arcorley528@gmail.com>
2017-08-23 12:06:34 -0700

    Update in_progress_detail.js

--- scripts/in_progress_detail.js
+++ scripts/in_progress_detail.js
@@ -2,6 +2,14 @@ var path = "https://www.wsusuperstudy.com/";
 
 $(document).ready(function(){
 
+	if ($("#disinterest").val() == 'no'){
+		$("#disinterest_reason_hidden").hide();
+	}
+
+	if ($("#ineligible_no_full_screen").val() == 'no'){
+		$("#ineligible_screen_hidden").hide();
+	}
+
 	/******** Hide/Show function for the referred by dropdown ********/
 	$("#referred_by").on('change', function(){
 		if ($("#referred_by").val() == "other"){
@@ -38,6 +46,28 @@ $(document).ready(function(){
 		}
 	});
 
+	/******** Hide/Show function for the ineligible no full screen reason dropdown ********/
+	$("#ineligible_no_full_screen").on('change',function(){
+		if ($("#ineligible_no_full_screen").val() == "yes"){
+			$("#ineligible_screen_hidden").show();
+		}
+		else{
+			$("#ineligible_screen_hidden").hide();
+			$("#ineligible_no_full_screen_reason").val('none');
+		}
+	});
+
+	/******** Hide/Show function for the disinterest reason dropdown ********/
+	$("#disinterest").on('change',function(){
+		if ($("#disinterest").val() == "yes"){
+			$("#disinterest_reason_hidden").show();
+		}
+		else{
+			$("#disinterest_reason_hidden").hide();
+			$("#disinterest_reason").val('none');
+		}
+	});
+
 	/******** Eligibility function ********/
 	$(".eligibility_field").on('change',function(){
 		//declare variables for calculations
@@ -50,6 +80,8 @@ $(document).ready(function(){
 		var livingTogetherLength = (livingTogetherYrs * 12) + livingTogetherMos;
 		var completedScreen = $("#completed_screen").val();
 		var ineligibleReason = '';
+		var ineligibleScreen = $("#ineligible_no_full_screen").val();
+		var disinterest = $("#disinterest").val();
 
 		/*****Assign Participant Pain Level*****/
 		var pptDailyPain = $("#ppt_daily_pain").val();
@@ -145,7 +177,17 @@ $(document).ready(function(){
 		}
 
 		/*****Pain Status Check*****/
-		if (pptPainStatus != 'Not Significant' && partnerPainStatus != 'Not Significant'){
+		if (ineligibleScreen == 'yes'){
+			$("#eligibility_status").val('no');
+			ineligibleReason = 'Participant is ineligible without full screen';
+			return;
+		}
+		else if (disinterest == 'yes'){
+			$("#eligibility_status").val('no');
+			ineligibleReason = 'Participant is not interested';
+			return;
+		}
+		else if (pptPainStatus != 'Not Significant' && partnerPainStatus != 'Not Significant'){
 			$("#eligibility_status").val('no');
 			ineligibleReason = 'Both partners have significant or chronic pain';
 			console.log(ineligibleReason);
